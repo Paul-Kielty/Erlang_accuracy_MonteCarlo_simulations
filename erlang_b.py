e61ce5f18d1d1c0e0b5c770a38261570fa9d60d7
Paul-Kielty <p.kielty3@nuigalway.ie>
2020-12-05 13:06:02 +0000

    created exponential call duration distribution

--- erlang_b.py
+++ erlang_b.py
@@ -1,8 +1,11 @@
 from math import factorial
+import matplotlib.pyplot as plt
+import numpy as np
+MEAN_CALL_HOLDING_TIME = 2/60 # HRS
 
 def offeredTraffic(calls_per_hour, hours_per_call): # A0
     return calls_per_hour*hours_per_call
-    
+
 def erlangb(n, A0):
     denom = 0
     for i in range(n):
@@ -10,5 +13,16 @@ def erlangb(n, A0):
     E1 = (A0**n)/(factorial(n))/denom
     return E1
 
-print(erlangb(39,30))
-print(erlangb(4, 0.595))
+# print(erlangb(39,30))
+# print(erlangb(4, 0.595))
+
+def trafficSimulation(numCalls):
+    calls = np.random.exponential(scale=MEAN_CALL_HOLDING_TIME, size=numCalls)
+    print(calls.mean())
+    plt.hist(calls, bins=1000)
+    # plt.plot(calls)
+    plt.title("Call duration frequency")
+    # plt.ylabel=("frequency")
+    plt.show()
+
+trafficSimulation(10000)
